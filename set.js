05580af84b285255f8e9b4b9ead42aa4047535ba
sadio2025 <sadio.sukuna@gmail.com>
2025-05-21 13:57:27 +0000

    Update set.js

--- set.js
+++ set.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
+  PREFIXE: process.env.PREFIXE || "."
+  NOM_OWNER: process.env.NOM_OWNER || "⫷𖣘𝐀₮𝐨m̲𝚒ᨶ☯️υ𝗸ꪊ𝘯𝖺𖣘⫸",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "224611422863",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_8JiFCF8P_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "⫷𖣘𝐀₮𝐨m̲𝚒ᨶ☯️υ𝗸ꪊ𝘯𝖺𖣘⫸",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "⫷𖣘𝐀₮𝐨m̲𝚒ᨶ☯️υ𝗸ꪊ𝘯𝖺𖣘⫸",
   DATABASE: process.env.DATABASE,
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
